e6cdd80a3d1800bcf9ad88a07f12eadba143e633
tasniaarhamarham <tasniaarhamarham@gmail.com>
2023-08-03 21:21:55 +0600

    Vercel_Deploay

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("First_Job_Task");
     const Form = database.collection("FormData");
@@ -91,8 +91,8 @@ console.log(result);
 res.send(result);
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
